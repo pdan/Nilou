9c7a3ca625f1eec6ff2c01f3e2e2f9b85aab97b6
Pourya Daneshvar <pourya@daneshvar.me>
2021-11-22 12:38:02 +0330

    Add Tailwind styles

---
+++ src/components/Board/index.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import './Board.css'
+
+
+interface Props {
+
+}
+
+const Board = ({ }: Props): JSX.Element => {
+
+    return (<div className="">
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+            Base board
+        </div>
+    </div>)
+}
+
+export default Board
