9751bcb3d7bf59b48eabe6a00a2a41074bf388e0
Pourya Daneshvar <pourya@daneshvar.me>
2021-11-29 16:55:42 +0330

    Delete store.ts

--- src/models/store.ts
+++
@@ -1,9 +0,0 @@
-import { Tile } from './Tile'
-export type Store = {
-    /* 
-        Tiles which is changed by user actions.
-        It will be cleaned when user update the map 
-    */
-
-    tilesStore: Tile[]
-}
